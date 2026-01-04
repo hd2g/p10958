0c1954d88c711a85d1598e528533e49f404f388b
hd2g <>
2026-01-04 13:47:30 +0900

    [Comb] Create

---
+++ src/Comb.hs
@@ -0,0 +1,29 @@
+module Comb where
+
+import Prelude
+
+-- import Operator
+
+-- import Data.List (intercalate)
+
+operators :: [Char]
+operators = ['+', '-', '*', '/', '^', ' ']
+
+sourceAsc :: String
+sourceAsc = "1 2 3 4 5 6 7 8 9"
+
+allCombinations :: String -> [String]
+allCombinations s = combine source
+  where
+    source :: [String]
+    source = words s
+
+    combine :: [String] -> [String]
+    combine [] = []
+    combine [x] = [x]
+    combine (x:xs) =
+      [ unary ++ x ++ (if op == ' ' then [] else [op]) ++ rest
+      | op <- operators
+      , unary <- ["", "-"]
+      , rest <- combine xs
+      ]
