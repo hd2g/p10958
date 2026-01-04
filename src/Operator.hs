c4b336fbb776e967078a2e6ed428eacc4dc0fe45
hd2g <>
2026-01-04 13:48:36 +0900

    [Operator] Number type is Double

--- src/Operator.hs
+++ src/Operator.hs
@@ -16,13 +16,13 @@ data BinaryOp
   deriving (Show, Eq)
 
 data Expr
-  = Number Int
+  = Number Double
   | Unary UnaryOp Expr
   | Binary BinaryOp Expr Expr
   deriving (Show, Eq)
 
 showExpr :: Expr -> String
-showExpr (Number n) = show n
+showExpr (Number n) = show $ floor n
 showExpr (Unary Plus e) = "+" ++ showExpr e
 showExpr (Unary Minus e) = "-" ++ showExpr e
 showExpr (Binary Add lhs rhs) = "(" ++ showExpr lhs ++ " + " ++ showExpr rhs ++ ")"
@@ -31,17 +31,16 @@ showExpr (Binary Mul lhs rhs) = "(" ++ showExpr lhs ++ " * " ++ showExpr rhs ++ 
 showExpr (Binary Div lhs rhs) = "(" ++ showExpr lhs ++ " / " ++ showExpr rhs ++ ")"
 showExpr (Binary Pow lhs rhs) = "(" ++ showExpr lhs ++ " ^ " ++ showExpr rhs ++ ")"
 
-eval :: Expr -> Int
+eval :: Expr -> Double
 eval (Number n) = n
 eval (Unary Plus e) = eval e
 eval (Unary Minus e) = - (eval e)
 eval (Binary Add rhs lhs) = eval rhs + eval lhs
 eval (Binary Sub rhs lhs) = eval rhs - eval lhs
 eval (Binary Mul rhs lhs) = eval rhs * eval lhs
-eval (Binary Div rhs lhs) = eval rhs `div` eval lhs
-eval (Binary Pow rhs lhs) = eval rhs ^ eval lhs
+eval (Binary Div rhs lhs) = eval rhs / eval lhs
+eval (Binary Pow rhs lhs) = eval rhs ** eval lhs
 
 exampleExpr :: Expr
 exampleExpr =
   Binary Add (Number 3) (Binary Mul (Unary Minus (Number 5)) (Number 2))
-
