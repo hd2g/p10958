ffbafe1fe539def6e0cb3d1b808fb86594479bbe
hd2g <>
2026-01-04 13:58:48 +0900

    [Comb] Add Anotation comments

--- src/Comb.hs
+++ src/Comb.hs
@@ -5,9 +5,11 @@ import Prelude
 operators :: [Char]
 operators = ['+', '-', '*', '/', '^', ' ']
 
+-- REFACTOR: Replace type to Text
 sourceAsc :: String
 sourceAsc = "1 2 3 4 5 6 7 8 9"
 
+-- REFACTOR: Replace type to Text
 allCombinations :: String -> [String]
 allCombinations s = combine source
   where
