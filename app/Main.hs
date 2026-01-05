dace7b48b655ef6e25e1a94eb720055ea86f0750
hd2g <>
2026-01-05 21:58:04 +0900

    [Main] print results, not write to file

--- app/Main.hs
+++ app/Main.hs
@@ -8,11 +8,8 @@ import Text.Printf (printf)
 import Comb (allCombinations, sourceAsc)
 import Solve (solve, found)
 
-distFilePath :: FilePath
-distFilePath = "./out.txt"
-
 main :: IO ()
 main = do
   let inputs = allCombinations sourceAsc
   for_ (zip inputs (solve inputs))  $ \ (input, result) -> do
-    appendFile distFilePath $ printf "%s\t%f\n" (input :: String) (result :: Double)
+    putStrLn $ printf "%s\t%f" (input :: String) (result :: Double)
